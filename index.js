213d9e249e85d8e5ae939d8bb33db7bcbad90698
Andrew <53106989+vorxcomp@users.noreply.github.com>
2021-03-15 13:13:37 -0700

    assets & notifications

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 const discURL = "https://www.amazon.com/gp/product/B08FC5L3RG";
 const digitalURL = "https://www.amazon.com/dp/B08FC6MR62"
 
+const path = require('path');
+
+const discImg = path.join(__dirname, '/assets/disc.png');
+const digitalImg = path.join(__dirname, '/assets/digital.png');
+
+
 const {Builder, By, Key, until} = require('selenium-webdriver');
+const notifier = require('node-notifier');
+const open = require('open');
+const { isContext } = require('vm');
+
+
+/**
+ * Scrape Amazon's listing for digital and disc version of PS5s
+ * Determine inventory if element exists, such as add to cart button
+ * Once inventory is found, stop the interval loop for ~1min and re-run to check for more stock. */
+
+function sendNotification(type, reason) { // accepts 2 params, type (disc/digital) & reason, if product found, notify end user
+    notifier.notify({
+        title: `(${type}) Amazon Web Scraper`,
+        message: `Found PS5 inventory\nReason: ${reason}`,
+        icon: (type == "Disc" ? discImg : digitalImg), // ternary operation to see what img to use from type
+        wait: true
+      });
+
+      notifier.on('click', function (notifierObject, options, event) {
+        open(type == "Disc" ? discURL : digitalURL);
+      });
+}
 
 
 function checkDigital() {
-    driver.findElement(webdriver.By.id('test')).then(function(webElement) {
-        console.log('Element exists');
-    }, function(err) {
-        if (err.state && err.state === 'no such element') {
-            console.log('Element not found');
-        } else {
-            webdriver.promise.rejected(err);
-        }
-    });
+
+    sendNotification("Digital", "Add-to-Cart Element")
 }
 
 function checkDisc() {
@@ -22,9 +43,11 @@ function checkDisc() {
 
 
 
-setInterval(function() {
-    let currentTime = new Date().toLocaleTimeString()
+// setInterval(function() {
+//     let currentTime = new Date().toLocaleTimeString()
 
-    checkDigital()
+    
 
-}, 1000)
+// }, 1000)
+
+checkDigital()
